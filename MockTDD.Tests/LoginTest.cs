850e200d410de460da4521b3150fe5462891d25d
unknown <admin@LENOVO-PC.intergrupo.com>
2015-05-04 10:26:35 -0500

    Initial commit

---
+++ MockTDD.Tests/LoginTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+using Moq;
+
+namespace MockTDD.Tests
+{
+    [TestClass]
+    public class LoginTest
+    {
+        private LoginService _loginService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var mockLoginRepository = new Mock<ILoginRepository>();
+            mockLoginRepository.Setup(r => r.Login("user", "user")).Returns(true);
+            mockLoginRepository.Setup(r => r.Login("wrong", "wrong")).Returns(false);
+
+            this._loginService = new LoginService(mockLoginRepository.Object);
+        }
+
+        [TestMethod]
+        public void ItCanLogin()
+        {
+            // Arrange
+            string username = "user";
+            string password = "user";
+            bool expectated = true;
+
+            // Act
+            bool result = this._loginService.login(username, password);
+
+            // Assert
+            Assert.AreEqual(expectated, result);
+        }
+
+        [TestMethod]
+        public void ItCantLoginWhenUserAndPasswordAreIncorrect()
+        {
+            // Arrange
+            string username = "wrong";
+            string password = "wrong";
+            bool expectated = false;
+
+            // Act
+            bool result = this._loginService.login(username, password);
+
+            // Assert
+            Assert.AreEqual(expectated, result);
+        }
+    }
+}
---
+++ Services/LoginService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class LoginService
+    {
+        private ILoginRepository _loginRepository;
+
+        public LoginService(ILoginRepository loginRepository)
+        {
+            this._loginRepository = loginRepository;
+        }
+
+        public bool login(string username, string password)
+        {
+            return this._loginRepository.Login(username, password);
+        }
+    }
+}
